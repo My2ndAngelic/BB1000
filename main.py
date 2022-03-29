64392f2963cc595329132593cf8da97bbdbf2f7c
My2ndAngelic <8387105+My2ndAngelic@users.noreply.github.com>
2022-03-30 00:36:12 +0200

    A1

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
+def caesar_shift(text, n):
+    """
+    Returns an encrypted string by shifting input characters by n
+    """
+    # YOUR CODE HERE
+    return ''.join(list(map(lambda x: chr(x), list(
+        map(lambda x: (ord(x) - 65 + n) % 26 + 65 if 65 <= ord(x) <= 96 else (ord(x) - 97 + n) % 26 + 97,
+            [text[x:x + 1] for x in range(0, len(text), 1)])))))
+    # return list(map(lambda x : (ord(x) + n) % 26, text.split()))
+    ################
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def mysum(sequence):
+    # from functools import reduce
+    # return reduce(lambda x, y: x + y, sequence)
+    return sequence[0] if len(sequence) == 1 else sequence[0] + mysum(sequence[1::])
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(caesar_shift('ABBA', 2))
+    print(caesar_shift('abba', 2))
+    print(mysum([1, 3, 5, 7]))
