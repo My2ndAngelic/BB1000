50e62c73b711ba9205eeb00078ed7da037bbd40d
My2ndAngelic <8387105+My2ndAngelic@users.noreply.github.com>
2022-03-31 01:08:51 +0200

    A1

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ def caesar_shift(text, n):
     Returns an encrypted string by shifting input characters by n
     """
     # YOUR CODE HERE
-    return ''.join(list(map(lambda x: chr(x), list(
-        map(lambda x: (ord(x) - 65 + n) % 26 + 65 if 65 <= ord(x) <= 96 else (ord(x) - 97 + n) % 26 + 97,
-            [text[x:x + 1] for x in range(0, len(text), 1)])))))
+    return ''.join(list(map(lambda x: chr(x), list(map(lambda x: (ord(x) + n - ((1 << 6) + (1 << 0))) % ((1 << 4) + (1 << 3) + (1 << 1)) + ((1 << 6) + (1 << 0)) if ((1 << 6) + (1 << 0)) <= ord(x) <= ((1 << 6) + (1 << 4) + (1 << 3) + (1 << 1) + (1 << 0)) else (ord(x)  + n - ((1 << 6) + (1 << 5) + (1 << 0))) % ((1 << 4) + (1 << 3) + (1 << 1)) + ((1 << 6) + (1 << 5) + (1 << 0)) if ((1 << 6) + (1 << 5) + (1 << 0)) <= ord(x) <= ((1 << 6) + (1 << 5) + (1 << 4) + (1 << 3) + (1 << 1) + (1 << 0)) else ord(x), [text[x:x + (1 << 0)] for x in range(0, len(text))])))))
     # return list(map(lambda x : (ord(x) + n) % 26, text.split()))
     ################
 
